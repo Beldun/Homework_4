0e6ea1f73e21082836c8199994864a2ae2446856
Beldun <scrimix_re@icloud.com>
2023-05-27 02:50:21 +0600

    hw4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullName: '',
+      phoneNumber: '',
+      email: '',
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { fullName, phoneNumber, email } = this.state;
+
+    console.log('ФИО:', fullName);
+    console.log('Номер телефона:', phoneNumber);
+    console.log('Email:', email);
+
+    alert(`ФИО: ${fullName}\nНомер телефона: ${phoneNumber}\nEmail: ${email}`);
+
+    this.setState({
+      fullName: '',
+      phoneNumber: '',
+      email: '',
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { fullName, phoneNumber, email } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          ФИО:
+          <input
+            type="text"
+            name="fullName"
+            value={fullName}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Номер телефона:
+          <input
+            type="text"
+            name="phoneNumber"
+            value={phoneNumber}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Отправить</button>
+      </form>
+    );
+  }
 }
 
-export default App;
+export default RegistrationForm;
